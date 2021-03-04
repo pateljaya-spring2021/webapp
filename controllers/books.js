247ebc6da98b139321c65d22ff72a3054fb21b61
Jayashree Patel <patel.jaya@husky.neu.edu>
2021-03-03 23:56:04 -0500

    adding images to book

--- controllers/books.js
+++ controllers/books.js
@@ -1,5 +1,7 @@
 const bookService = require("../services/books");
 const { bookWithoutUpdatedDate } = require("../utils/helper");
+const File = require("../models").File;
+const { reject } = require("lodash");
 
 const createBook = (req, res) => {
   const userId = req.user.id;
@@ -56,9 +58,94 @@ const getAllBooks = (req, res) => {
     });
 };
 
+const addBookImage = async (req, res) => {
+  if (!req.file) {
+    res.status(400).send({
+      message: "No File Uploaded!",
+    });
+  }
+
+  const book_id = req.book.id;
+  const user_id = req.user.id;
+  const file_name = new Date().getMilliseconds() + "_" + req.file.originalname;
+  const file_path = req.file.path;
+
+  File.findAll({
+    where: { book_id: book_id, file_name: file_name },
+  }).then((files) => {
+    if (files.length > 0) {
+      reject(
+        res.status(400).send({
+          message: "Duplicate file name not allowed",
+        })
+      );
+    } else {
+      if (isValidImage(file_name)) {
+        bookService
+          .addBookImageToS3(file_path, file_name)
+          .then((data) => {
+            const removeHttpregex = /(^\w+:|^)\/\//;
+            const s3Obj = data.Location.replace(removeHttpregex, "");
+
+            bookService
+              .createFile(file_name, s3Obj, user_id, book_id)
+              .then((file) => {
+                res.status(201).send(file);
+              })
+              .catch((error) => {
+                res.status(400).send(error);
+              });
+          })
+          .catch((error) => {
+            res.status(400).send(error);
+          });
+      } else {
+        res.status(400).send({
+          message: "Unsupported File Type",
+        });
+      }
+    }
+  });
+};
+
+const isValidImage = (file_name) => {
+  const imageRegex = /\.(gif|jpe?g|png)$/i;
+  return imageRegex.test(file_name);
+};
+
+const deleteBookImage = (req, res) => {
+  File.findByPk(req.params.image_id)
+    .then((file) => {
+      if (file) {
+        bookService
+          .deleteBookImageFromS3(file.dataValues.file_name)
+          .then((s3_data) => {
+            bookService
+              .deleteBookImage(file.dataValues.file_id)
+              .then(() => {
+                res.sendStatus(204);
+              })
+              .catch((errors) => {
+                res.status(400).json(errors);
+              });
+          })
+          .catch((error) => {
+            res.status(400).send(error);
+          });
+      } else {
+        res.status(404).send({ message: "No such image found" });
+      }
+    })
+    .catch((errorResponse) => {
+      reject(fromattedErrors(errorResponse));
+    });
+};
+
 module.exports = {
   createBook,
   getBook,
   deleteBook,
-  getAllBooks
+  getAllBooks,
+  addBookImage,
+  deleteBookImage,
 };
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -8,6 +8,22 @@ const booksController = require("../controllers/books");
 const authenticateUser = require("../middleware/userAuthentication");
 const authorizeUser = require("../middleware/authorizeUser");
 
+const fs = require("fs");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const path = `./images/`;
+    fs.mkdirSync(path, { recursive: true });
+    cb(null, path);
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().getTime() + "_" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
 /**
  * The following route is used to create a book
  * @method POST
@@ -16,11 +32,7 @@ const authorizeUser = require("../middleware/authorizeUser");
  * @param {Object} req request
  * @param {Object} res response
  */
-bookRouter.post(
-  "/",
-  authenticateUser,
-  booksController.createBook
-);
+bookRouter.post("/", authenticateUser, booksController.createBook);
 
 /**
  * The following route is used to get book by id
@@ -40,7 +52,12 @@ bookRouter.get("/:id", booksController.getBook);
  * @param {Object} req request
  * @param {Object} res response
  */
-bookRouter.delete("/:id", authenticateUser, authorizeUser, booksController.deleteBook);
+bookRouter.delete(
+  "/:id",
+  authenticateUser,
+  authorizeUser,
+  booksController.deleteBook
+);
 
 /**
  * The following route is used get all books
@@ -51,5 +68,29 @@ bookRouter.delete("/:id", authenticateUser, authorizeUser, booksController.delet
  * @param {Object} res response
  */
 bookRouter.get("/", booksController.getAllBooks);
+
+/**
+ * The following route is used to add images to book
+ * @method POST
+ * @memberof bookRoute
+ * @function /books
+ * @param {Object} req request
+ * @param {Object} res response
+ */
+
+bookRouter.post(
+  "/:book_id/image",
+  authenticateUser,
+  authorizeUser,
+  upload.single("file_name"),
+  booksController.addBookImage
+);
+
+bookRouter.delete(
+  "/:id/image/:image_id",
+  authenticateUser,
+  authorizeUser,
+  booksController.deleteBookImage
+);
 
 module.exports = bookRouter;
