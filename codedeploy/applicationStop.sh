5c86dc491a6840e61e33edd9e6bd8c068c71da4a
Jayashree Patel <patel.jaya@husky.neu.edu>
2021-03-14 18:02:48 -0400

    test

--- codedeploy/applicationStop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script is used to stop application
-# echo "*******Stopped the cloudwatch service*********"
-# sudo systemctl stop cloudwatch.service
-
-echo "*******Kill Node Service*********"
-lsof -ti tcp:3005 | xargs kill
