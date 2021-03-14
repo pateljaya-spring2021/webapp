615158d6cfea31d497942d2c2be3f10b4a69c549
Jayashree Patel <patel.jaya@husky.neu.edu>
2021-03-14 17:35:50 -0400

    test

--- codedeploy/applicationStop.sh
+++ codedeploy/applicationStop.sh
@@ -1,3 +1,8 @@
-#!/usr/bin/env bash
-cd ~/node
-pm2 stop www || true
+#!/bin/bash
+
+# This script is used to stop application
+# echo "*******Stopped the cloudwatch service*********"
+# sudo systemctl stop cloudwatch.service
+
+echo "*******Kill Node Service*********"
+killall -s KILL node
