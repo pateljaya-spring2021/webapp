666bbc69943db024ad729b89c67d0bee8b16ff43
Jayashree Patel <patel.jaya@husky.neu.edu>
2021-03-13 22:47:54 -0500

    changes in config file

--- config/config.js
+++ config/config.js
@@ -8,10 +8,10 @@ module.exports = {
     dialect: "mysql",
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.MYSQL_HOST,
     dialect: "mysql",
   },
   production: {
