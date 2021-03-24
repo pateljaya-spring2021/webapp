b92229d9a927748d34a91448e48456a88e1ac83c
Jayashree Patel <patel.jaya@husky.neu.edu>
2021-03-23 23:39:08 -0400

    adding cloudwatch configuration and logs

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -2,4 +2,5 @@
 
 cd /home/ubuntu/webapp/webapp
 pwd
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp/webapp/amazon-cloudwatch-agent.json -s
 npm run start-ec2-webapp
